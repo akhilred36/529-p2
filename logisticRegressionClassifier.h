0de17867c3bd1662e9fb0b5e4bbd34761e1eac1d
MuricanWizard <herculesroxx@gmail.com>
2022-03-14 17:06:08 -0600

    start logistic regression

---
+++ logisticRegressionClassifier.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <utility> // pair
+#include <stdexcept> // runtime_error
+#include <sstream> // stringstream
+#include <time.h>
+#include <chrono>
+#include <stdlib.h>
+#include <math.h>  
+#include <unordered_map>
+#include "pythonpp.h"
+
+
+using namespace std;
+
+class logisticRegression{
+
+};
