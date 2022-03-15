7db93dd29cb8a4be3bd2a3dc1b34ac221fa2c492
akhilred36 <akhilred36@gmail.com>
2022-03-15 11:06:15 -0600

    clean up main

--- main.cpp
+++ main.cpp
@@ -30,27 +30,5 @@ int main(int argc, char** argv){
 
     end = chrono::steady_clock::now();
     std::cout << "Total time for reading and predicting = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
-
-    // vector<vector<int>> data;
-    // chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    // data = read_csv_int("../training.csv");
-    // chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    // std::cout << "Time to read file = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
-
-    // vector<string> vocab;
-    // unordered_map<string, int> vocab_dict;
-    // vocab = read_lines("../vocabulary.txt");
-    // vocab_dict = make_dict(vocab);
-    // cout << "The index of " << vocab[932] << " is " << vocab_dict[vocab[932]] << endl;
-
-    // vector<string> label_vocab;
-    // unordered_map<string, int> label_vocab_dict;
-    // label_vocab = read_lines("../newsgrouplabels.txt");
-    // label_vocab_dict = make_dict(label_vocab, 1);
-    // cout << "The index of " << label_vocab[15] << " is " << label_vocab_dict[label_vocab[15]] << endl;
-
-    //Read in logProbMatrix
-    // vector<vector<double>> logProbMatrix;
-    // logProbMatrix = read_csv_double("logProbMatrix.mtx");
     return 0;
 }
